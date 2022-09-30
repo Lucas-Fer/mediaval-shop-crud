07610cab1063eadb6ccd6931ed60037ef08bea7e
lucas <fernandes20lucas@gmail.com>
2022-09-30 17:25:29 -0300

    Reiniciando o projeto

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,7 @@
 import express from 'express';
-import ProductsRoutes from './routes/productsRoute';
-import UsersRoutes from './routes/userRoutes';
-import OrdersRoutes from './routes/ordersRoute';
 
 const app: express.Application = express();
 
 app.use(express.json());
-
-app.use('/products', ProductsRoutes);
-app.use('/users', UsersRoutes);
-app.use('/orders', OrdersRoutes);
 
 export default app;
--- src/interfaces/usersInterface.ts
+++
@@ -1,9 +0,0 @@
-interface IUser {
-  id?: number,
-  username: string,
-  classe: string,
-  level: string,
-  password?: string,
-}
-
-export default IUser;
